f0fba39b54908924b347b9658867cc766c62f74d
Subin Lee <46069735+subin9126@users.noreply.github.com>
2021-10-21 18:59:19 +0900

    Update Train_and_Predict_glmnet.m

--- Train_and_Predict_glmnet.m
+++ Train_and_Predict_glmnet.m
@@ -16,19 +16,19 @@ options = glmnetSet; options.standardize = 1;
 options.alpha = 0.5;   % 0 = L1; 1 = L2 
 
 %-------------------------------------------------------------------------
-
+% Train:
 numTrainSubjects = size(TrainDataset,1); 
 
 CVIdx = crossvalind('Kfold', numTrainSubjects, nfolds);
 
-% Train:
 CVerr = cvglmnet(TrainDataset, TrainLabel,'binomial', options, 'auc', nfolds, CVIdx, false, true, true);
                  % x, y, family, options, type, nfolds, goldid, parallel, keep, grouped                 
 
 % Save model parameters:
 ModelParam = [CVerr.glmnet_fit.a0(find(CVerr.lambda == CVerr.lambda_min)); ...
               CVerr.glmnet_fit.beta(:,find(CVerr.lambda == CVerr.lambda_min))];
-          
+
+%-------------------------------------------------------------------------
 % Predict:
 Logits = cvglmnetPredict(CVerr, PredictionDataset, CVerr.lambda_min, 'link');
                          % object, newx, s, type
